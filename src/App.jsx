279acfa4c81cf7d2f11c7129b799b9b947ca4aeb
Rishabh Kumrawat <97178821+rishikumrawat02@users.noreply.github.com>
2024-03-01 15:22:32 +0000

    Updated Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Rishabh Kumrawat",
+  title: "Web Developer & Competitive Programmer",
+  email: "Rishabh.Kumrawat@studentambassadors.com",
+  gitHub: "https://github.com/rishikumrawat02",
+  instagram: "https://www.instagram.com/rishi_kumrawat_/",
+  linkedIn: "https://www.linkedin.com/in/rishabh-kumrawat/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Instagram Clone",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Instagram clone is a digital platform designed to replicate the core functionalities and user experience of the popular social media app, Instagram.",
+    url: "https://github.com/rishikumrawat02/Instagram-Clone",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Food Ordering App",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Fool Ordering App is a user-friendly app with features such as login/register, efficient cart management, diverse menus, real-time order tracking, and an intuitive admin panel.",
+    url: "https://github.com/rishikumrawat02/Food-Delivery-App",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://rishabhkumrawat.in/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Idea-Fi",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Idea-Fi is an app that gamifies intellectual property education, helping school and college students preserve their creativity and business ideas by imparting knowledge about intellectual property rights in an engaging manner.",
+    url: "https://github.com/rishikumrawat02/IdeaFi-Backend",
   },
 ];
 
